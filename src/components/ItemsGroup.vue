6a0181a91735435f66aa6a82b3145d2f5e0222e6
AhGang <474494594@qq.com>
2019-07-21 13:47:54 +0800

    finish basic requirment

--- src/components/ItemsGroup.vue
+++ src/components/ItemsGroup.vue
@@ -1,28 +1,42 @@
 <template>
 <div>
-<div>
+  <Card class="div-card">
+<div class="div-header">
+  <div class="div-header-title">
   <h1>Jquery To Do List</h1>
 </div>
-<div>
+<div class="div-header-comment">
   <h3>Simple Todo List with adding and filter by diff status.</h3>
 </div>
-<div>
-  <div>
-     <input type="text" v-model="num">
-    <button @click="addItem" >Add</button>
-    <div v-for="(el, index) in showItems" :key="index">
-      <span>{{index+1}}.</span>
-       <input type="checkbox" @click="selecetdItem(index)"/>{{el}}
+</div>    
+<div class="div-body">
+     <Input class="div-body-input" type="text" v-model="itemName"></Input>
+    <Button type="success" class="div-body-add-button"    @click="addItem" >Add</Button>
+    <div class="div-body-data-table">
+      <div class="div-body-data-table-outer">
+    <div  class="div-body-data-table-single" v-for="(el, index) in showItems" :key="index" >
+      <span  class="div-body-data-table-del" v-if="el.isSelected">{{index+1}}.</span>
+      <span v-else>{{index+1}}.</span> 
+      <span>
+         <Checkbox class="div-body-data-table-checkbox" @click="completedItem(index)" v-model="el.isSelected">
+           <del v-if="el.isSelected" class="div-body-data-table-del" >{{el.val}}</del>
+      <span v-else>{{el.val}}</span>
+         </Checkbox>
+      
+      </span>
+     
     </div>
+      </div>
+      </div> 
   </div>
-  <div>
-    <button >ALL</button>
-    <button @click="showItems()">Active</button>
-    <button >Complete</button>
+  <div class="div-footer">
+    <Button type="text" class="div-footer-button"  @click="showItemsStatus('All')">All</Button>
+    <Button type="text" class="div-footer-button"  @click="showItemsStatus('Active')">Active</Button>
+    <Button type="text" class="div-footer-button"  @click="showItemsStatus('Complete')">Complete</Button>
   </div>
+  </Card>
 </div>
 
-</div>
 </template>
 
 <script>
@@ -30,26 +44,46 @@ export default {
  name: 'ItemsGroup',
  data(){
     return {
-      num:0,
+      itemName:"",
       allItems: [],
-      showItems: []
+      showItems: [],
+      status: 'All'
     }
   },
   methods:{
    addItem(){
-     item = {val:num,isSelected:false}
+     if(this.itemName != ""){
+       let item = {
+       val:this.itemName,
+       isSelected: this.status =='Complete'?true:false
+       }
      this.allItems.push(item)
      this.showItems.push(item)
+     this.itemName = ""
+     }else{
+        this.$Message.error('Can not add a null item')
+     }
    },
-   selecetdItem(index){
-    this.selectedItems[index].isSelected = true
+   completedItem(index){
+     if(!this.showItems[index].isSelected){
+      this.showItems[index].isSelected = true
+      this.allItems[index].isSelected = true
+     }else{
+      this.showItems[index].isSelected = false
+      this.allItems[index].isSelected = false
+     }
    },
-   showActivshowItemseItems(){
-    this.showItems.filter(e=>(e.selecetdItem = false))
+   showItemsStatus(status){
+     this.status = status
+     if(status=='Active'){
+        this.showItems = this.allItems.filter(e=>(e.isSelected == false))
+     }else if(status=='Complete'){
+        this.showItems = this.allItems.filter(e=>(e.isSelected == true))
+     }else{
+        this.showItems = this.allItems;
+     }
+   
    }
 }
 }
 </script>
-
-<style scoped>
-</style>
