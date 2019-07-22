eaab7f15dd3e79d9f8d94fdcc2a2349fdef1162c
AhGang <474494594@qq.com>
2019-07-22 20:01:37 +0800

    use computed to avoid update vueX data directly

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,14 +1,14 @@
 <template>
       <div  v-bind:class="{'div-body-data-item':itemData.index % 2 == 0}">
-         <span  class="div-body-data-table-del" v-if="itemData.el.isSelected">{{itemData.index+1}}.</span>
+         <span  class="div-body-data-table-del" v-if="item.isSelected">{{itemData.index+1}}.</span>
       <span v-else>{{itemData.index+1}}. </span> 
       <span>
-         <Checkbox size="large" class="div-body-data-table-checkbox" @click="completedItem(itemData.index)" v-model="itemData.el.isSelected">  </Checkbox>
-         <span v-if="!itemData.el.isEditing" @dblclick="editItemName(itemData.index)">
-          <del v-if="itemData.el.isSelected" class="div-body-data-table-del" >{{itemData.el.val}}</del>
-        <span v-else>{{itemData.el.val}}</span>  
+         <Checkbox size="large" class="div-body-data-table-checkbox" @click="completedItem(itemData.index)" v-model="item.isSelected">  </Checkbox>
+         <span v-if="!item.isEditing" @dblclick="editItemName(itemData.index)">
+          <del v-if="item.isSelected" class="div-body-data-table-del" >{{item.val}}</del>
+        <span v-else>{{item.val}}</span>  
          </span>
-       <Input v-else v-model="itemData.el.val" :autofocus="true" class="div-body-data-table-input" size="small" @on-blur="itemInputOnBlur(itemData.index)" @on-enter="itemInputOnBlur(itemData.index)"></Input>
+       <Input v-else v-model="item.val" :autofocus="true" class="div-body-data-table-input" size="small" @on-blur="itemInputOnBlur(itemData.index)" @on-enter="itemInputOnBlur(itemData.index)"></Input>
        </span>
       </div>
 </template>
@@ -21,6 +21,11 @@ data(){
      
     }
   },
+ computed:{
+    item:function(){
+       return this.$store.state.showItems[this.itemData.index]
+    }
+ }, 
  methods:{
     completedItem(index){
      this.$emit("completedItem",index)
