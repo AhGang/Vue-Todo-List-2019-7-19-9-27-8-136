4f52a3bec32573e40962e60e53c5197e4bb9606c
AhGang <474494594@qq.com>
2019-07-22 15:50:20 +0800

    引入 vue x

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import iView from 'iview';
+import store from './store/index.js'
 import 'iview/dist/styles/iview.css';
 Vue.config.productionTip = false
 
 Vue.use(iView)
 new Vue({
+  store
   render: h => h(App),
 }).$mount('#app')
